95415d79f28ddcf29ef02fa516c8c19ab9ba84cc
Javier Miron <javiermiron89@gmail.com>
2022-02-16 14:52:07 -0600

    Se carga la clase 5

---
+++ Clase5/src/Datos.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author javie
+ */
+public class Datos {
+    
+    private String Nombre;
+    private int Edad;
+    
+    public Datos(String Nombre, int Edad){
+        this.Nombre = Nombre;
+        this.Edad = Edad;
+    }
+
+    /**
+     * @return the Nombre
+     */
+    public String getNombre() {
+        return Nombre;
+    }
+
+    /**
+     * @param Nombre the Nombre to set
+     */
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    /**
+     * @return the Edad
+     */
+    public int getEdad() {
+        return Edad;
+    }
+
+    /**
+     * @param Edad the Edad to set
+     */
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+    
+}
