1ebadc7b0db1fa46db93351c35d1d16e6958f5a7
Javier Miron <javiermiron89@gmail.com>
2022-04-06 12:42:19 -0600

    Clase 10

---
+++ ClienteServidor/server/src/routes/indexRoutes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+
+class IndexRouter {
+    public router: Router = Router();
+
+    constructor() {
+        this.config();
+    }
+
+    config(): void {
+        this.router.get('/hola', (req, res) => res.send("Hola Mundo"));
+    }
+}
+
+const indexRouter = new IndexRouter();
+export default indexRouter.router;
